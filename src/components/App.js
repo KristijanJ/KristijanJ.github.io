0f07ae4597bda38ac23ac978d3ac1fd1a606fec0
Kristijan Jovanovski <kristijan.j.mail@gmail.com>
2019-12-03 12:05:03 +0100

    react app for homepage test

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h3>Hello World</h3>
+      <p>Kristijan Jovanovski</p>
+    </div>
+  );
+}
+
+export default App;
