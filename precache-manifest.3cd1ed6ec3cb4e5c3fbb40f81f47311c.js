de53feb47d2d36a21cb24d7198c3668933d906a2
Kristijan Jovanovski <kristijan.j.mail@gmail.com>
2019-12-03 12:09:31 +0100

    pushing built react app to master branch

---
+++ precache-manifest.3cd1ed6ec3cb4e5c3fbb40f81f47311c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4eef984198cdc0e8fbfedff81dee04b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "844e96e31febe91f359f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "85a7de68d8c026493545",
+    "url": "/static/js/2.a0d26d1a.chunk.js"
+  },
+  {
+    "revision": "844e96e31febe91f359f",
+    "url": "/static/js/main.fcd2d84e.chunk.js"
+  },
+  {
+    "revision": "35c25030d1ad6b0509be",
+    "url": "/static/js/runtime-main.47d82ecc.js"
+  }
+]);
