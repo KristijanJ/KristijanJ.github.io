db775bcd5af1a0a808b79cf53cb618065beaa928
Kristijan Jovanovski <kristijan.j.mail@gmail.com>
2020-02-10 11:11:12 +0100

    added new portfolio

---
+++ precache-manifest.4e1fa24d3d1d25b952574af9670dce5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031f5ffb829deba80c5807463438b425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cc08cf31a185d3d3ba0",
+    "url": "/static/css/main.86333b15.chunk.css"
+  },
+  {
+    "revision": "de71a021eb173f2cec5c",
+    "url": "/static/js/2.ba7a40a0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ba7a40a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8cc08cf31a185d3d3ba0",
+    "url": "/static/js/main.422c11df.chunk.js"
+  },
+  {
+    "revision": "2c15b6a860b07c5ef893",
+    "url": "/static/js/runtime-main.80b66699.js"
+  }
+]);
